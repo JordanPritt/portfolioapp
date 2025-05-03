eb644e11f7db019b8f76f66d4f1831311594f4c0
Jordan <pritt.jordan@gmail.com>
2025-05-03 19:12:06 -0400

    Update to tailwind config.

--- pritt-portfolio/tailwind.config.ts
+++ pritt-portfolio/tailwind.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
     content: [
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,16 +9,18 @@ module.exports = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                outfit: ['var(--font-outfit)'],
+                ovo: ['var(--font-ovo)'],
+            },
             colors: {
                 lightHover: '#fcf4ff',
                 darkHover: '#2a004a',
                 darkTheme: '#11001f'
             },
-            fontFamily: {
-                outfit: ['var(--font-outfit)'],
-                ovo: ['var(--font-ovo)'],
-            },
         },
     },
     plugins: [],
-};
+}
+
+export default config
